c193b451b221f37cba4a8f6755b27d00fe9b5331
RafaelMavili <97104243+RafaelMavili@users.noreply.github.com>
2022-01-15 05:53:30 +0100

    add funcao mapearTeclado para usar as teclas do teclado

--- calculadora.js
+++ calculadora.js
@@ -15,7 +15,7 @@ const operacaoPendente = () => operador !== undefined;
 
 const calcular = () => {
     if (operacaoPendente()) {
-        const numeroAtual = parseFloat(display.textContent);
+        const numeroAtual = parseFloat(display.textContent.replace(',', '.'));
         novoNumero = true;
         //eval envez da verificao com o if else
         const resultado = eval(`${numeroAnterior}${operador}${numeroAtual}`);
@@ -63,7 +63,7 @@ const selecionarOperador = (evento) => {
         novoNumero = true;
         operador = evento.target.textContent;
         //vai guardar o operador e numeroAnterior depois d click
-        numeroAnterior = parseFloat(display.textContent);
+        numeroAnterior = parseFloat(display.textContent.replace(',', '.'));
     }
 }
 
@@ -95,3 +95,60 @@ document.getElementById('limparCalculo').addEventListener('click', limparCalculo
 const removerUltimoCaractere = () => display.textContent = display.textContent.slice(0, -1);
 
 document.getElementById('backspace').addEventListener('click', removerUltimoCaractere);
+
+//* -1, vai pegar o numero e inverter o sinal do mesmo
+const inverterSinal = () => {
+    novoNumero = true;
+    atualizarDisplay(display.textContent * -1);
+}
+
+document.getElementById('inverter').addEventListener('click', inverterSinal);
+
+//true quando for diferent d -1,  e false quando igual a -1
+const existeDecimal = () => display.textContent.indexOf(',') !== -1;
+
+const existeValor = () => display.textContent.length > 0;
+
+const inserirDecimal = () => {
+    //so pode inserir quando nao existe virgula
+    if (!existeDecimal()) {
+        if (existeValor()) {
+            atualizarDisplay(',');
+        } else {
+            atualizarDisplay('0,');
+        }
+    }
+}
+
+document.getElementById('decimal').addEventListener('click', inserirDecimal);
+
+const mapaTeclado = {
+    '0': 'tecla0',
+    '1': 'tecla1',
+    '2': 'tecla2',
+    '3': 'tecla3',
+    '4': 'tecla4',
+    '5': 'tecla5',
+    '6': 'tecla6',
+    '7': 'tecla7',
+    '8': 'tecla8',
+    '9': 'tecla9',
+    '/': 'operadorDividir',
+    '*': 'operadorMultiplicar',
+    '-': 'operadorSubtrair',
+    '+': 'operadorAdicionar',
+    '=': 'igual',
+    'Enter': 'igual',
+    'Backspace': 'backspace',
+    'c': 'limparDisplay',
+    'Espace': 'limparCalculo',
+    ',': 'decimal'
+}
+const mapearTeclado = (evento) => {
+    const tecla = evento.key;
+    //verifica se uma das chaves do objeto mapaTeclado tem a tecla precionado
+    const teclaPermitida = () => Object.keys(mapaTeclado).indexOf(tecla) !== -1;
+    if (teclaPermitida()) document.getElementById(mapaTeclado[tecla]).click();
+}
+
+document.addEventListener('keydown', mapearTeclado);
